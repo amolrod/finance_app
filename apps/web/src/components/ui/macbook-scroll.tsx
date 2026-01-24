e5012a4cf6797a929b37d8a27247c9043831f90b
angeelmr <angelmolinarodriguez15@gmail.com>
2026-01-24 22:39:33 +0100

    Clamp macbook screen travel

--- apps/web/src/components/ui/macbook-scroll.tsx
+++ apps/web/src/components/ui/macbook-scroll.tsx
@@ -66,7 +66,8 @@ export function MacbookScroll({ src, showGradient, title, badge, children }: Mac
 
   const scaleX = useTransform(scrollYProgress, [0, 0.3], [1.2, isMobile ? 1 : 1.5]);
   const scaleY = useTransform(scrollYProgress, [0, 0.3], [0.6, isMobile ? 1 : 1.5]);
-  const translate = useTransform(scrollYProgress, [0, 1], [0, 1500]);
+  const maxTranslate = isMobile ? 750 : 1000;
+  const translate = useTransform(scrollYProgress, [0, 1], [0, maxTranslate]);
   const rotate = useTransform(scrollYProgress, [0.1, 0.12, 0.3], [-28, -28, 0]);
   const textTransform = useTransform(scrollYProgress, [0, 0.3], [0, 100]);
   const textOpacity = useTransform(scrollYProgress, [0, 0.2], [1, 0]);
