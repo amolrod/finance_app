6a50b26033f49c7835f24c62591ab5263c3ca4c4
angeelmr <angelmolinarodriguez15@gmail.com>
2026-01-24 23:20:52 +0100

    Increase parallax bottom space

--- apps/web/src/app/como-funciona/page.tsx
+++ apps/web/src/app/como-funciona/page.tsx
@@ -467,7 +467,7 @@ export const HeroParallax = ({ products }: { products: Product[] }) => {
   return (
     <div
       ref={ref}
-      className="h-[300vh] py-40 overflow-hidden antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
+      className="h-[300vh] pt-40 pb-[40vh] overflow-hidden antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
     >
       <Header />
       <motion.div
